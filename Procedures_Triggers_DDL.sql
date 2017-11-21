d5fc062a7eb3988b9c5f904b0472db822e0b101c
arjunssharma <32558983+arjunssharma@users.noreply.github.com>
2017-11-20 23:39:30 -0500

    Refactored Triggers and Queries

--- Procedures_Triggers_DDL.sql
+++ Procedures_Triggers_DDL.sql
@@ -1,3 +1,67 @@
+drop PROCEDURE SELECT_PROFESSOR_OPTIONS;
+drop PROCEDURE INSERT_QUESTION_AND_RETURN_ID;
+drop PROCEDURE INSERT_EXERCISE_AND_RETURN_ID;
+drop PROCEDURE INSERT_AS_AND_RETURN_ID;
+drop sequence ATTEMPT_SUBMISSION_SEQ;
+drop sequence Question_SEQ;
+drop sequence EXERCISE_SEQ;
+ drop trigger ATTEMPT_SUBMISSION_PK_Trigger;
+ drop trigger EXERCISE_PK_Trigger;
+ drop trigger Question_PK_Trigger;
+drop trigger check_is_grad;
+drop trigger check_not_TA;
+drop trigger Points_Scoring_Policy;
+drop trigger Average_Difficulty_Level;
+drop trigger check_TA_is_enrolled;
+ 
+drop table adaptive_exercise_topic;
+drop table TA;
+drop table COURSE_STUDENT;
+drop table SUBMISSION_RESULT;---
+drop table ATTEMPT_SUBMISSION;
+drop table QUESTION_PARAM_ANSWERS;--
+drop table ANSWER;
+drop table PARAMETER;
+drop table QUESTION_BANK;
+drop table EXERCISE_QUESTION;
+drop table QUESTION;
+drop table exercise;
+Drop table COURSE_topic;
+Drop table topic;
+Drop table COURSE;
+Drop table student;
+Drop table professor;
+drop table ROLE;
+drop table MENU_OPTIONS;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 CREATE TABLE MENU_OPTIONS 
 (
   ROLE VARCHAR(1) NOT NULL 
@@ -136,6 +200,7 @@ CREATE SEQUENCE  EXERCISE_SEQ  MINVALUE 1 MAXVALUE 9999999999999999999999999999 
 
 
 
+
 -----------------------------------------------------------------------------
 CREATE TABLE EXERCISE 
 (
@@ -143,7 +208,8 @@ CREATE TABLE EXERCISE
 , COURSE_ID VARCHAR(20) NOT NULL 
 , NAME VARCHAR(100) 
 , DEADLINE DATE 
-, TOTAL_QUESTIONS NUMBER 
+, TOTAL_QUESTIONS NUMBER
+, CURRENT_QUESTION_COUNT NUMBER DEFAULT 0
 , RETRIES NUMBER 
 , START_DATE DATE 
 , END_DATE DATE 
@@ -151,7 +217,7 @@ CREATE TABLE EXERCISE
 , PENALTY NUMBER 
 , SCORING_POLICY VARCHAR(20) 
 , SC_MODE VARCHAR(20)
-, DIFFICULTY_LEVEL NUMBER
+, DIFFICULTY_LEVEL NUMBER DEFAULT 0
 , CONSTRAINT EXERCISE_PK PRIMARY KEY 
   (
     EXERCISE_ID 
@@ -173,13 +239,11 @@ ON DELETE SET NULL
 -----------------------------------------------------------------------------
 CREATE SEQUENCE  QUESTION_SEQ  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 ;
 
-
-
 -----------------------------------------------------------------------------
 CREATE TABLE QUESTION 
 (
   QUESTION_ID NUMBER NOT NULL 
-, QUESTION_TEXT VARCHAR(200) NOT NULL 
+, QUESTION_TEXT VARCHAR(500) NOT NULL 
 , DIFFICULTY_LEVEL NUMBER NOT NULL 
 , HINT VARCHAR(200) 
 , EXPLANATION VARCHAR(200) 
@@ -326,14 +390,13 @@ ON DELETE CASCADE
 CREATE SEQUENCE  ATTEMPT_SUBMISSION_SEQ  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 ;
 
 
-
 -----------------------------------------------------------------------------
 CREATE TABLE ATTEMPT_SUBMISSION 
 (
   ATTEMPT_ID NUMBER NOT NULL  
 , EXERCISE_ID NUMBER NOT NULL 
 , STUDENT_ID VARCHAR(20) NOT NULL 
-, SUBMISSION_TIME TIMESTAMP NOT NULL 
+, SUBMISSION_TIME TIMESTAMP 
 , POINTS NUMBER 
 , NUMBER_OF_ATTEMPTS NUMBER
 , SCORE NUMBER
@@ -496,22 +559,9 @@ ON DELETE CASCADE
 
 
 -----------------------------------------------------------------------------
-CREATE OR REPLACE TRIGGER check_is_grad
-  BEFORE INSERT OR UPDATE ON TA
-  FOR EACH ROW
-DECLARE
-  isGrad number;
-BEGIN
-select is_grad into isGrad from student s where s.STUDENT_ID = :NEW.STUDENT_ID and rownum = 1;
-if ( isGrad <> 1)
---if ( :NEW.STUDENT_ID >= 1)
-then 
-Raise_Application_Error(-20000, 'Undergrads cannot be TA');
---insert into tp values(1);
-END IF;
-END;
-/ 
-ALTER TRIGGER check_is_grad ENABLE;
+
+
+
 
 CREATE OR REPLACE TRIGGER EXERCISE_PK_Trigger 
    before insert on  EXERCISE
@@ -528,7 +578,12 @@ ALTER TRIGGER EXERCISE_PK_Trigger ENABLE;
 
 
 
+
+
+
 ----------------------------------------------------------------------------------------
+
+
 CREATE OR REPLACE TRIGGER Question_PK_Trigger 
    before insert on  Question
    for each row 
@@ -541,6 +596,11 @@ begin
 end;
 / 
 ALTER TRIGGER Question_PK_Trigger ENABLE;
+
+
+
+
+
 
 
 
@@ -557,6 +617,27 @@ begin
 end;
 /
 ALTER TRIGGER ATTEMPT_SUBMISSION_PK_Trigger ENABLE;
+
+
+
+
+---------------------------------------------------------------------
+CREATE OR REPLACE TRIGGER check_is_grad
+  BEFORE INSERT OR UPDATE ON TA
+  FOR EACH ROW
+DECLARE
+  isGrad number;
+BEGIN
+select is_grad into isGrad from student s where s.STUDENT_ID = :NEW.STUDENT_ID and rownum = 1;
+if ( isGrad <> 1)
+--if ( :NEW.STUDENT_ID >= 1)
+then 
+Raise_Application_Error(-20000, 'Undergrads cannot be TA');
+--insert into tp values(1);
+END IF;
+END;
+/ 
+ALTER TRIGGER check_is_grad Enable;
 
 
 
@@ -605,16 +686,71 @@ CREATE OR REPLACE TRIGGER Average_Difficulty_Level
 AFTER DELETE OR INSERT ON EXERCISE_QUESTION 
 FOR EACH ROW
 DECLARE
-Average number;
+init_average number;
+init_count number;
+new_difficulty number;
 BEGIN
-  Select AVG(DIFFICULTY_LEVEL) into Average from QUESTION where QUESTION_ID in(Select QUESTION_ID from EXERCISE_QUESTION where EXERCISE_ID = :NEW.EXERCISE_ID);
-  If(Average>0) then
-  UPDATE EXERCISE SET DIFFICULTY_LEVEL = Average  WHERE EXERCISE_ID =:NEW.EXERCISE_ID;
-  end if; 
+  Select CURRENT_QUESTION_COUNT into init_count from EXERCISE where EXERCISE_ID = :NEW.EXERCISE_ID;
+  Select  DIFFICULTY_LEVEL into init_average from EXERCISE where EXERCISE_ID = :NEW.EXERCISE_ID;
+  Select DIFFICULTY_LEVEL into new_difficulty from QUESTION where QUESTION_ID = :NEW.QUESTION_ID;
+  UPDATE EXERCISE SET DIFFICULTY_LEVEL = (((init_average * init_count) + new_difficulty)/(init_count + 1))  WHERE EXERCISE_ID =:NEW.EXERCISE_ID;
+  UPDATE EXERCISE SET CURRENT_QUESTION_COUNT = (CURRENT_QUESTION_COUNT + 1) where  EXERCISE_ID =:NEW.EXERCISE_ID;
 END;
 /
 
-ALTER TRIGGER Average_Difficulty_Level DISABLE;
+ALTER TRIGGER Average_Difficulty_Level enable;
+
+--------------------------------------------------------------------------
+
+create or replace TRIGGER Points_Scoring_Policy
+BEFORE INSERT ON ATTEMPT_SUBMISSION 
+--AFTER INSERT ON ATTEMPT_SUBMISSION
+FOR EACH ROW
+DECLARE
+does_exist varchar(3);
+points number;
+policy VARCHAR(20);
+no_of_attempts number;
+BEGIN
+  --SELECT count(*)  into countcheck from  ATTEMPT_SUBMISSION where EXERCISE_ID = :NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID;
+  select 
+  case when exists(select 1 from ATTEMPT_SUBMISSION where EXERCISE_ID =:NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID)
+  then 'y'
+  else
+  'n'
+  end
+  into does_exist from COURSE where ROWNUM=1;
+  if does_exist = 'n'
+  then :NEW.SCORE := :NEW.POINTS;
+  else
+  Select SCORING_POLICY into policy from EXERCISE where EXERCISE_ID = :NEW.EXERCISE_ID;
+  If(policy = 'MAX') then
+  SELECT 
+   max(POINTS)
+    into points FROM ATTEMPT_SUBMISSION where EXERCISE_ID = :NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID GROUP BY EXERCISE_ID,STUDENT_ID;
+  if points < :NEW.points
+  then :NEW.score := :NEW.points;
+  end if;
+  end if;
+  If(policy = 'AverageScore') then
+  SELECT 
+   avg(POINTS)
+    into points FROM ATTEMPT_SUBMISSION where EXERCISE_ID = :NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID GROUP BY EXERCISE_ID,STUDENT_ID;
+  select 
+  MAX(NUMBER_OF_ATTEMPTS)
+  into no_of_attempts from ATTEMPT_SUBMISSION where EXERCISE_ID = :NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID;
+  :NEW.score := ((points * no_of_attempts) + :NEW.POINTS)/(no_of_attempts + 1);
+  end if;
+  If(policy = 'LatestAttempt') then
+  --SELECT POINTS into points FROM ATTEMPT_SUBMISSION where EXERCISE_ID = :NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID ORDER BY ATTEMPT_ID DESC;
+  :NEW.score := :NEW.POINTS;
+  end if;
+  end if;
+  --UPDATE ATTEMPT_SUBMISSION SET SCORE = points  WHERE EXERCISE_ID =:NEW.EXERCISE_ID AND STUDENT_ID= :NEW.STUDENT_ID;
+END;
+ /
+
+ALTER TRIGGER Points_Scoring_Policy enable;
 
 
 
@@ -654,4 +790,77 @@ begin
 insert into EXERCISE (COURSE_ID, NAME, DEADLINE, TOTAL_QUESTIONS, RETRIES, START_DATE, END_DATE, POINTS, PENALTY, SCORING_POLICY, SC_MODE ) values (COURSE_ID, NAME,TO_DATE(DEADLINE, 'MM/DD/YYYY'), TOTAL_QUESTIONS,RETRIES,TO_DATE(START_DATE, 'MM/DD/YYYY'), TO_DATE(END_DATE, 'MM/DD/YYYY'), POINTS, PENALTY, SCORING_POLICY, SC_MODE ) returning EXERCISE_ID into ret_id;
 open prc for select ret_id as ret_id from EXERCISE where rownum = 1;
 end;
-/ 
+/
+----------------------------------------------------------------------------------------
+CREATE OR REPLACE PROCEDURE INSERT_AS_AND_RETURN_ID(exid IN NUMBER, username IN VARCHAR, dt IN VARCHAR ,totalpoints IN NUMBER,attemptno IN NUMBER,prc OUT sys_refcursor)
+AS
+ret_id number;
+begin
+insert into ATTEMPT_SUBMISSION (EXERCISE_ID, STUDENT_ID, SUBMISSION_TIME, POINTS, NUMBER_OF_ATTEMPTS ) values (exid, username,TO_DATE(dt, 'MM/DD/YYYY'), totalpoints,attemptno ) returning ATTEMPT_ID into ret_id;
+open prc for select ret_id as ret_id from ATTEMPT_SUBMISSION where rownum = 1;
+end;
+/
+----------------------------------------
+
+CREATE OR REPLACE PROCEDURE RETURN_REPORT(COURSEID IN VARCHAR,prc OUT sys_refcursor)
+AS
+begin
+open prc for 
+select es.e_id as Ex, es.s_id as St, es.sname as Nm, es.ename as eNm,
+case when asu.score is null then 0 else asu.score end as score from
+(select e.EXERCISE_ID as e_id,e.NAME as ename ,cs.STUDENT_ID as s_id, s.name as sname
+from 
+EXERCISE e, COURSE_STUDENT cs, STUDENT s
+where e.COURSE_ID = cs.COURSE_ID
+and cs.STUDENT_ID = s.STUDENT_ID
+and e.COURSE_ID = COURSEID
+) es
+left join ATTEMPT_SUBMISSION asu on (asu.EXERCISE_ID = es.e_id and asu.STUDENT_ID = es.s_id)
+where asu.NUMBER_OF_ATTEMPTS is null or asu.NUMBER_OF_ATTEMPTS = (select max(asu2.NUMBER_OF_ATTEMPTS) from ATTEMPT_SUBMISSION asu2 where asu2.EXERCISE_ID =es.e_id and asu2.STUDENT_ID = es.s_id )
+order by Ex, St;
+end;
+
+drop sequence ATTEMPT_SUBMISSION_SEQ;
+drop sequence Question_SEQ;
+drop sequence EXERCISE_SEQ;
+
+declare
+    las_new_seq INTEGER;
+begin
+   select max(attempt_id) + 1
+   into   las_new_seq
+   from   attempt_submission;
+
+    execute immediate 'Create sequence attempt_submission_seq
+                       start with ' || las_new_seq ||
+                       ' increment by 1';
+end;
+/
+
+declare
+    lq_new_seq INTEGER;
+begin
+   select max(question_id) + 1
+   into   lq_new_seq
+   from   question;
+
+    execute immediate 'Create sequence question_seq
+                       start with ' || lq_new_seq ||
+                       ' increment by 1';
+end;
+
+/
+
+declare
+    l_new_seq INTEGER;
+begin
+   select max(exercise_id) + 1
+   into   l_new_seq
+   from   exercise;
+
+    execute immediate 'Create sequence exercise_seq
+                       start with ' || l_new_seq ||
+                       ' increment by 1';
+end;
+
+/
